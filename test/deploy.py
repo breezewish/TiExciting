d466ca067058122b22644d184336af0b3d9033c3
blackbbc <505968815@qq.com>
2019-10-26 10:09:16 -0700

    wip

--- test/deploy.py
+++ test/deploy.py
@@ -60,43 +60,43 @@ def write_to_file(path, content):
 
 def deploy(config):
     # 1. 下载sha256
-    task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.sha256 force=yes' % TIDB_SHA256_URL)
-    print(task.get_result())
+    # task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.sha256 force=yes' % TIDB_SHA256_URL)
+    # print(task.get_result())
 
-    # 2. 取出sha256
-    task = AnsibleTask('shell', "cat /tmp/tidb.sha256|awk '{print $1}'")
-    result = task.get_result()
-    print(result)
-    sha256 = result['success']['localhost']['stdout']
+    # # 2. 取出sha256
+    # task = AnsibleTask('shell', "cat /tmp/tidb.sha256|awk '{print $1}'")
+    # result = task.get_result()
+    # print(result)
+    # sha256 = result['success']['localhost']['stdout']
 
-    task = AnsibleTask('stat', 'checksum_algorithm=sha256 path=/tmp/tidb.tar.gz')
-    result = task.get_result()
-    print(result)
-    if not result['exists'] or result['checksum'] != sha256:
-        # 3. 下载大礼包并检查sha256
-        task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.tar.gz force=yes checksum=sha256:%s' % (TIDB_URL, sha256))
-        print(task.get_result())
+    # task = AnsibleTask('stat', 'checksum_algorithm=sha256 path=/tmp/tidb.tar.gz')
+    # result = task.get_result()
+    # print(result)
+    # if not result['exists'] or result['checksum'] != sha256:
+    #     # 3. 下载大礼包并检查sha256
+    #     task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.tar.gz force=yes checksum=sha256:%s' % (TIDB_URL, sha256))
+    #     print(task.get_result())
 
-    # 4. 解压大礼包
-    task = AnsibleTask('shell', 'tar -xzf /tmp/tidb.tar.gz')
-    print(task.get_result())
+    # # 4. 解压大礼包
+    # task = AnsibleTask('shell', 'tar -xzf /tmp/tidb.tar.gz')
+    # print(task.get_result())
 
-    # 5. 分发大礼包
-    # pd   => /tmp/{{TIDB_DIR_NAME}}/bin/pd-server
-    # tikv => /tmp/{{TIDB_DIR_NAME}}/bin/tikv-server
-    # tidb => /tmp/{{TIDB_DIR_NAME}}/bin/tidb-server
-    pd_path = '/tmp/%s/bin/pd-server' % TIDB_DIR_NAME
-    tikv_path = '/tmp/%s/bin/tikv-server' % TIDB_DIR_NAME
-    tidb_path = '/tmp/%s/bin/tidb-server' % TIDB_DIR_NAME
+    # # 5. 分发大礼包
+    # # pd   => /tmp/{{TIDB_DIR_NAME}}/bin/pd-server
+    # # tikv => /tmp/{{TIDB_DIR_NAME}}/bin/tikv-server
+    # # tidb => /tmp/{{TIDB_DIR_NAME}}/bin/tidb-server
+    # pd_path = '/tmp/%s/bin/pd-server' % TIDB_DIR_NAME
+    # tikv_path = '/tmp/%s/bin/tikv-server' % TIDB_DIR_NAME
+    # tidb_path = '/tmp/%s/bin/tidb-server' % TIDB_DIR_NAME
 
-    task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/pd/ mode=0755' % pd_path, 'pd_servers')
-    print(task.get_result())
+    # task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/pd/ mode=0755' % pd_path, 'pd_servers')
+    # print(task.get_result())
 
-    task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/tikv/ mode=0755' % tikv_path, 'tikv_servers')
-    print(task.get_result())
+    # task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/tikv/ mode=0755' % tikv_path, 'tikv_servers')
+    # print(task.get_result())
 
-    task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/tidb/ mode=0755' % tidb_path, 'tidb_servers')
-    print(task.get_result())
+    # task = AnsibleTask('copy', 'src=%s dest=~/TiExciting/tidb/ mode=0755' % tidb_path, 'tidb_servers')
+    # print(task.get_result())
 
     # 6. 生成执行脚本
 
@@ -112,18 +112,18 @@ def deploy(config):
     for server_ip in ['192.168.233.129', '192.168.233.130', '192.168.233.131']:
         write_to_file('/tmp/launch.sh', gen_tikv_script('pd_xiaohou', '/home/tidb/data1', server_ip, 20160, 20180,
                                                         [('192.168.233.128', 2380)]))
-        task = AnsibleTask('copy', 'src=/tmp/launch.sh dest=/home/tidb/TiExciting/pd/ mode=0755', server_ip)
+        task = AnsibleTask('copy', 'src=/tmp/launch.sh dest=/home/tidb/TiExciting/tikv/ mode=0755', server_ip)
         print(task.get_result())
 
     task = AnsibleTask('shell', 'bash /home/tidb/TiExciting/tikv/launch.sh', 'tikv_servers', True)
     print(task.get_result())
 
     # tidb-servers
-    write_to_file('/tmp/launch.sh', gen_tidb_script('pd_xiaohou', '/home/tidb/data1', '192.168.233.128', 4000, 10080,
+    write_to_file('/tmp/launch.sh', gen_tidb_script('pd_xiaohou', '/home/tidb/data2', '192.168.233.128', 4000, 10080,
                                                     [('192.168.233.128', 2380)]))
     task = AnsibleTask('copy', 'src=/tmp/launch.sh dest=/home/tidb/TiExciting/tidb/ mode=0755', '192.168.233.128')
     print(task.get_result())
-    task = AnsibleTask('shell', 'bash /home/tidb/TiExciting/pd/launch.sh', 'tidb_servers', True)
+    task = AnsibleTask('shell', 'bash /home/tidb/TiExciting/tidb/launch.sh', 'tidb_servers', True)
     print(task.get_result())
 
 
