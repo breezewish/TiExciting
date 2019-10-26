ca73f66f9869d9d62e477e4489e27b45f17806dd
Sweet <505968815@qq.com>
2019-10-26 16:18:14 +0800

    wip

--- test/deploy.py
+++ test/deploy.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.append('..')
 
-from ansible.ansible_task import AnsibleTask
+from ansible_util.ansible_task import AnsibleTask
 
 TIDB_VERSION = 'v3.0.3'
 TIDB_URL = 'http://download.pingcap.org/tidb-%s-linux-amd64.tar.gz' % TIDB_VERSION
@@ -14,10 +14,11 @@ TIDB_SHA256_URL = 'http://download.pingcap.org/tidb-%s-linux-amd64.sha256' % TID
 def deploy():
     task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.sha256 force=yes' % TIDB_SHA256_URL, 'localhost')
     print(task.get_result())
-    task = AnsibleTask('shell', "cat /tmp/tidb.sha256|awk '{print $1}'")
+    task = AnsibleTask('shell', "cat /tmp/tidb.sha256|awk '{print $1}'", 'localhost')
+    sha256 = task['success']['localhost']['stdout']
     print(task.get_result())
-    # task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.tar.gz force=yes checksum' % TIDB_URL, 'localhost')
-    # print(task.get_result())
+    task = AnsibleTask('get_url', 'url=%s dest=/tmp/tidb.tar.gz force=yes checksum=%s' % (TIDB_URL, sha256), 'localhost')
+    print(task.get_result())
 
 
 if __name__ == '__main__':
